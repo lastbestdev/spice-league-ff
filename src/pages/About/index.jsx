7d871a68eac2f80044f23ef89e89f3ed8aea6bb5
Brad Deibert <bradeibert7@gmail.com>
2024-07-03 16:52:05 -0600

    add commish pic

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,4 +1,9 @@
+import commishPic from '../../assets/comish_pic.webp'
+import style from './style.module.css'
+
 export function About() {
+	const isMobile = window.innerWidth < 639
+
 	return (
 		<section style="page">
 			<h1>About 🧐</h1>
@@ -10,10 +15,18 @@ export function About() {
 				<li><strong>2023-24: Quentin Foust</strong> (record: 11-3, 1st round: Derrick Henry)</li>
 			</ul>
 
-			<h2>Your Commissioner</h2>
-			<h3>Brad Deibert</h3>
-			<p>Oftentimes affectionately referred to as "my commish" by league members, Brad has served as full time commissioner of the Spice League since filling the role in 2023. He loves all things fantasy football, Green Bay Packers, and especially grown men acting their age when it comes to their fantasy team. (Hint: it makes his job much easier!)</p>
-			<p>Brad lives in Missoula, MT with his wife, Alicia, and helplessly adorable housecat, Dorothy.</p>
+			<div class={style.commishHeader}>
+				<div>
+					<h2>Your Commissioner</h2>
+					{isMobile && <img src={commishPic} alt="commissioner pic" height="240" width="240" class={style.commishImg}/>}
+					<h3>Brad Deibert</h3>
+					<p>Oftentimes affectionately referred to as "my commish" by league members, Brad has served as full time commissioner of the Spice League since filling the role in 2023. He loves all things fantasy football, Green Bay Packers, and especially grown men acting their age when it comes to their fantasy team. (Hint: it makes his job much easier!)</p>
+					<p>Brad lives in Missoula, MT with his wife, Alicia, and helplessly adorable housecat, Dorothy.</p>
+				</div>
+				{!isMobile && <img src={commishPic} alt="commissioner pic" height="240" width="240" class={style.commishImg}/>}
+			</div>
+
+			
 		</section>
 	);
 }
