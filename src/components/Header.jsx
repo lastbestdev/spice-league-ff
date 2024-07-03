186f9b99dc2b9dca5a56b5e95d0bca38f032266d
Brad Deibert <bradeibert7@gmail.com>
2024-07-03 14:28:50 -0600

    nav styles done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,16 @@
-import { useLocation } from 'preact-iso';
 import spiceLogo from '../assets/ff_logo_secondary.png'
+import style from './header.module.css'
 
 export function Header() {
-	const { url } = useLocation();
-
 	return (
-		<header>
-			<a href="/">
-					<img src={spiceLogo} alt="spice league logo" height="80" width="80" />
-				</a>
+		<header class={style.header}>
+			<a href="/" class={style.headerLeft}>
+				<img src={spiceLogo} alt="spice league logo" height="80" width="80" class={style.logo} />
+				<h2>Spice League Fantasy &nbsp; 🏈 🏆</h2>				
+			</a>
 			<nav>
-				<a href="/" class={url == '/' && 'active'}>
-					Home
+				<a alt="go back" onClick={() => history.back()} class={style.navlink}>
+					Back
 				</a>
 			</nav>
 		</header>
