1798e415cf3176934466f3bf310a76e149fc7b0a
Brad Deibert <bradeibert7@gmail.com>
2024-07-03 19:04:33 -0600

    will this work?

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
 const isDev = process.env.IS_DEVELOPMENT == "true";
 
-export const BASE_URL_PATH = isDev ? "/" : "/spice-league-ff/";
+export const BASE_URL_PATH = "/spice-league-ff/";
 export const getRelativeLink = (resource = "") => `${BASE_URL_PATH}${resource}`;
