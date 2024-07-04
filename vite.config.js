6c70fe7bede6e7b9a1b8c39db538fc8cf257e1bf
Brad Deibert <bradeibert7@gmail.com>
2024-07-03 18:26:33 -0600

    try

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
+  plugins: [preact()],
+  root: "./",
+  build: {
+    outDir: "dist",
+  },
+  base: process.env.GH_PAGES ? "/spice-league-ff/" : "/",
+  publicDir: "public",
 });
