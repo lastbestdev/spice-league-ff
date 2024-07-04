d38d54b77492700a6d40b9728584ef31b6ae961d
Brad Deibert <bradeibert7@gmail.com>
2024-07-03 18:59:09 -0600

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import spiceLogo from '../assets/ff_logo_secondary.png'
+import { getRelativeLink } from '../utils';
 import style from './header.module.css'
 
 export function Header() {
 	return (
 		<header class={style.header}>
-			<a href="/" class={style.headerLeft}>
+			<a href={getRelativeLink()} class={style.headerLeft}>
 				<img src={spiceLogo} alt="spice league logo" height="80" width="80" class={style.logo} />
 				<h2>Spice League Fantasy 🏈 🏆</h2>				
 			</a>
