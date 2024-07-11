f27d30432f46841473277d90d614239eb62db80b
Brad Deibert <44413459+braddeibert@users.noreply.github.com>
2024-07-11 08:25:07 -0600

    Update index.jsx

--- src/pages/Season/index.jsx
+++ src/pages/Season/index.jsx
@@ -6,11 +6,14 @@ export function Season() {
             <h2>League Openings</h2>
             <strong>1 position currently available! Please email bradeibert7@gmail.com with your name & reference (i.e. name of current league member referring you)</strong>
 
-   <h2>Pot Total: $600</h2>
+      <h2>Pot Total: $600</h2>
 
 			<h2>Buy-in & Pay-out</h2>
 			<p><strong>Buy-in:</strong> $50</p>
 			<p><strong>Pay-out:</strong> 80/20/$$$ back (i.e. 1st: 80% pot, 2nd: 20% pot, 3rd: $50)</p>
+
+   <h2>Last Place Punishment</h2>
+			<p>Eat 1 whole fresh Carolina Reaper pepper</p>
 
 			<h2>Important Dates</h2>
 			<ul>
